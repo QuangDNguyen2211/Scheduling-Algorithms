9aeaf146925d44b53edb26d00134f5ed55263395
QuangDNguyen2211 <70936038+QuangDNguyen2211@users.noreply.github.com>
2020-10-30 23:23:08 -0700

    Add files via upload

--- schedule_priority_rr.cpp
+++ schedule_priority_rr.cpp
@@ -10,12 +10,53 @@ using std::ignore;
 void SchedulePriorityRR::schedule(CPU *cpu)
 {
     cout << "SchedulePriorityRR::schedule()" << endl;
-    ignore = cpu;
+    int lowest_priority = tasks[0]->front()->priority;
+    int i = 1;
+
+    for (list<Task *> *queue : tasks)
+        for (Task *task : *queue)
+          if(task->priority > lowest_priority)
+            lowest_priority = task->priority;
+
+    while(!tasks[0]->empty()) {
+      Task *taskToRun = pickNextTask();
+      if(taskToRun->burst > cpu->QUANTUM)
+        cpu->run(taskToRun, cpu->QUANTUM);
+      else
+        cpu->run(taskToRun, taskToRun->burst);
+
+      taskToRun->burst -= cpu->QUANTUM;
+
+      if(taskToRun->burst <= 0) {
+        for (list<Task *> *queue : tasks)
+            for (Task *task : *queue)
+              if(task == taskToRun){
+                delete task;
+              }
+        tasks[0]->remove(taskToRun);
+      }
+      else{
+        if(taskToRun->priority == lowest_priority * i){
+          i++;
+        }
+        taskToRun->priority += lowest_priority * i;
+      }
+    }
 }
 
 Task *SchedulePriorityRR::pickNextTask()
 {
-    return NULL;
+  int highest_priority = tasks[0]->front()->priority;
+
+  for (list<Task *> *queue : tasks)
+      for (Task *task : *queue)
+        if(task->priority < highest_priority)
+          highest_priority = task->priority;
+
+  for (list<Task *> *queue : tasks)
+      for (Task *task : *queue)
+        if(task->priority == highest_priority)
+          return task;
 }
 
 Scheduler *create()
--- schedule_rr.cpp
+++ schedule_rr.cpp
@@ -9,44 +9,38 @@ using std::ignore;
 
 void ScheduleRR::schedule(CPU *cpu) {
   cout << "ScheduleRR::schedule()" << endl;
-  Task *task;
+  Task *taskToRun;
 
-  // Initialize the number of tasks in the list and assign to 'numberOfTasks'
-  int numberOfTasks = tasks[0]->size();
-  // Run all tasks based on the value of a quantum which is 10
-  for (int i = 0; i < numberOfTasks; i++) {
-    for (int j = 0; j < numberOfTasks; j++) {
-      // Get the selected task and prepare to run
-      task = pickNextTask();
-      // If the value of a specific task's burst is equal or greater than the value of a quantum
-      // Then that task will run fully 1 quantum which is 10
-      if (task->burst >= cpu->QUANTUM) {
-        cpu->run(task, cpu->QUANTUM);
-        task->burst -= cpu->QUANTUM;
-      } else { // If not, then that task will run based on the value of its burst
-        // That task will run exactly from the leftover value of its burst
-        if (task->burst > 0) {
-          cpu->run(task, task->burst);
-          task->burst = 0;
-        } else { // If the value of its burst is 0, which means that task is completed,
-                 // then it will not run anymore
-          continue;
-        }
-      }
+  while(!tasks[0]->empty()) {
+    taskToRun = pickNextTask();
+    if(taskToRun->burst > cpu->QUANTUM)
+      cpu->run(taskToRun, cpu->QUANTUM);
+    else
+      cpu->run(taskToRun, taskToRun->burst);
+
+    taskToRun->burst -= cpu->QUANTUM;
+
+    if(taskToRun->burst <= 0) {
+      for (list<Task *> *queue : tasks)
+          for (Task *task : *queue)
+            if(task == taskToRun){
+              delete task;
+            }
+      tasks[0]->remove(taskToRun);
+    }
+    else{
+      Task *temp = tasks[0]->front();
+      tasks[0]->pop_front();
+      tasks[0]->push_back(temp);
     }
   }
 }
 
 Task *ScheduleRR::pickNextTask() {
-  // Get the first task of 'sortedPriorityTasks' and assign to 'task'
-  Task *task = tasks[0]->front();
-  // Remove that task
-  tasks[0]->pop_front();
-  // Put it into the back of the list
-  tasks[0]->push_back(task);
-  return task;
+  return tasks[0]->front();
 }
 
-Scheduler *create(){
-  return new ScheduleRR;
+Scheduler *create()
+{
+    return new ScheduleRR;
 }
